f1152768cb9f1d467556f74bcf2362b746ee6e0f
«Vladimir <vjk1976@ukr.net>
2019-03-08 15:15:49 +0200

    it is working

--- main1.js
+++ main1.js
@@ -2,15 +2,21 @@
      let cpuNum = Math.floor(Math.random() * 10);
      console.log(cpuNum);
 
-     document.querySelector('.btn').oncklick = function(){
+     document.querySelector('.btn').onclick = function(){
 
-        let inNum = document.querySelector('.inNum').nodeValue;
+        let inNum = document.querySelector('.inNum').value;
         inNum = parseInt(inNum);
 
          if (!isNaN(inNum) && inNum >=0 && inNum <=10) {
 
             if(inNum == cpuNum) {
-                document.querySelector('.outresult').innerHTML= 'You have got it';
+                document.querySelector('.outResult').innerHTML= 'You have got it';
+                setTimeout(
+                    function(){
+                        location.reload();
+                    },
+                    3000
+                )
             }
             else {
                 document.querySelector('.outResult').innerHTML= 'Try again!';
